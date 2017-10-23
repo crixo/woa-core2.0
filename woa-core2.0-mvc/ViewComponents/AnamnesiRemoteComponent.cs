c3336d8c3c986aaeb5839ff23d32eab074175f61
Cristiano DeGiorgis <cristiano.degiorgis@deltatre.it>
2017-10-23 19:56:58 +0200

    ViewComponent sample

---
+++ woa-core2.0-mvc/ViewComponents/AnamnesiRemoteComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Woa.ViewComponents
+{
+    [ViewComponent(Name = "AnamnesiRemote")]
+    public class AnamnesiRemoteComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(int pazienteId)
+        {
+            return View();
+        }
+    }
+}
